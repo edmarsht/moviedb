c1659ef67d51ba87e1d57fed86b5f5c9c73b5be8
edmarsht <edtoulet@gmail.com>
2022-01-26 18:16:05 +0100

    card with data

--- src/component/movie/Movie.js
+++ src/component/movie/Movie.js
@@ -1,20 +1,37 @@
 import React from "react";
-import "./Movie.css"
+import "./Movie.css";
 
 function Movie({ movie }) {
+  const dateFormater = (date) => {
+    let [yy, mm, dd] = date.split("-");
+    return [dd, mm, yy].join("/");
+  };
   return (
     <div>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <div class="card__header-text">
-            <h3 class="card__title">{movie.title}</h3>            
+      <a href="" class="card">
+        <img
+          src={
+            movie.poster_path
+              ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
+              : "./image_default.png"
+          }
+          className="card__image"
+          alt="Poster movie"
+        />
+        <div className="card__overlay">
+          <div className="card__header">
+            <div className="card__header-text">
+              <h3 className="card__title">{movie.title}</h3>
+            </div>
           </div>
+          {movie.release_date ? (
+            <p className="card__description">Sorti le {dateFormater(movie.release_date)}</p>
+          ) : (
+            ""
+          )}
+          <h3 className="card__description">{movie.vote_average}/10 ☆</h3>
         </div>
-        <p class="card__description">{movie.overview}</p>
-      </div>
-    </a>      
+      </a>
     </div>
   );
 }
