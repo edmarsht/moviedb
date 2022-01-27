73e8410a1e5c2e632df7fe06160b778143ac1cb1
edmarsht <edtoulet@gmail.com>
2022-01-27 12:45:06 +0100

    work on card genre and description

--- src/component/movie/Movie.js
+++ src/component/movie/Movie.js
@@ -3,22 +3,93 @@ import { Link } from "react-router-dom";
 import "./Movie.css";
 
 function Movie({ movie }) {
+
   const dateFormater = (date) => {
     let [yy, mm, dd] = date.split("-");
     return [dd, mm, yy].join("/");
   };
+
+  const genreFinder = () => {
+    let genreArray = [];
+    for (let i = 0; i < movie.genre_ids.length; i++) {
+      switch (movie.genre_ids[i]) {
+        case 28:
+          genreArray.push(`Action`);
+          break;
+        case 12:
+          genreArray.push(`Aventure`);
+          break;
+        case 16:
+          genreArray.push(`Animation`);
+          break;
+        case 35:
+          genreArray.push(`Comédie`);
+          break;
+        case 80:
+          genreArray.push(`Policier`);
+          break;
+        case 99:
+          genreArray.push(`Documentaire`);
+          break;
+        case 18:
+          genreArray.push(`Drame`);
+          break;
+        case 10751:
+          genreArray.push(`Famille`);
+          break;
+        case 14:
+          genreArray.push(`Fantasy`);
+          break;
+        case 36:
+          genreArray.push(`Histoire`);
+          break;
+        case 27:
+          genreArray.push(`Horreur`);
+          break;
+        case 10402:
+          genreArray.push(`Musique`);
+          break;
+        case 9648:
+          genreArray.push(`Mystère`);
+          break;
+        case 10749:
+          genreArray.push(`Romance`);
+          break;
+        case 878:
+          genreArray.push(`Science-fiction`);
+          break;
+        case 10770:
+          genreArray.push(`Téléfilm`);
+          break;
+        case 53:
+          genreArray.push(`Thriller`);
+          break;
+        case 10752:
+          genreArray.push(`Guerre`);
+          break;
+        case 37:
+          genreArray.push(`Western`);
+          break;
+        default:
+          break;
+      }
+    }
+    // return <p>{genreArray[0]}</p>
+    return genreArray.slice(0, 3).map((genre) => <p key={genre}>{genre}</p>)
+  };
+
   return (
     <div>
       <a href="" class="card">
-          <img
-            src={
-              movie.poster_path
-                ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
-                : "./image_default.png"
-            }
-            className="card__image"
-            alt="Poster movie"
-          />
+        <img
+          src={
+            movie.poster_path
+              ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
+              : "./image_default.png"
+          }
+          className="card__image"
+          alt="Poster movie"
+        />
         <div className="card__overlay">
           <div className="card__header">
             <div className="card__header-text">
@@ -33,6 +104,8 @@ function Movie({ movie }) {
             ""
           )}
           <h3 className="card__description">{movie.vote_average}/10 ☆</h3>
+          <p className="card__description description">{movie.overview}</p>
+          <p className="card__description genre">{genreFinder()}</p>
         </div>
       </a>
     </div>
